370043319d7e2b1b723797978596640ab88be514
Jiaxin Wang <jiaxwang@umich.edu>
2019-02-28 01:56:03 -0500

    field draft

---
+++ Assets/Scripts/Field/GroundRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundRotation : MonoBehaviour
+{
+    float speed = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, 45 * Time.deltaTime * speed);
+    }
+}
