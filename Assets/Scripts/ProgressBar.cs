383e69454da239dcbcff30e80f4b45ef2ccdf2e9
金诚诚 <jcc@jinchengchengdeMacBook-Pro.local>
2019-02-27 23:54:22 -0500

    progress bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public float time = 1.0f;
+    private Slider slider;
+    private bool isCounting;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = 0;
+        isCounting = false;
+    }
+
+    void Update()
+    {
+        if (!isCounting)
+        {
+            isCounting = true;
+            StartCoroutine(StartCounting());
+        }
+        
+    }
+
+    IEnumerator StartCounting()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            slider.value += 0.01f;
+            yield return new WaitForSeconds(time / 100f);
+        }
+        isCounting = false;
+        gameObject.SetActive(false);
+    }
+}
