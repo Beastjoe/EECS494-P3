9beb32a695e32284ffdd59556955fb57decfd2e9
91017 <joezhong@umich.edu>
2019-02-28 02:05:33 -0500

    arrow key movement

---
+++ Assets/Scripts/Player/playerStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStatus : MonoBehaviour {
+
+  public enum status { DEFENSE, NORMAL, HOLDING, HELD, FLYING }
+  public status currStatus;
+
+  // Start is called before the first frame update
+  void Start() {
+    currStatus = status.NORMAL;
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
---
+++ Assets/Scripts/Player/shadowScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadowScript : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+    SpriteRenderer render = GetComponent<SpriteRenderer>();
+    render.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    render.receiveShadows = true;
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
