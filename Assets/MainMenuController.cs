63547bb97011dad005763f00c6577229e0e5f550
kinmark <kinmark@umich.edu>
2019-03-14 23:31:59 -0400

    welcome page

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Experimental.Input;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    private static int playerCount = 2;
+    bool[] hasPressed = new bool[playerCount];
+    public Text PressCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int i = 0; i < playerCount; i++)
+        {
+            hasPressed[i] = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        // Check the input
+        for (int i = 0; i < playerCount; i++)
+        {
+            Gamepad gp = Gamepad.all[i];
+            if (gp.xButton.isPressed)
+            {
+                hasPressed[i] = true;
+            }
+        }
+        
+        // Update the x count
+        int xCount = 0;
+        for (int i = 0; i < playerCount; i++)
+        {
+            if (hasPressed[i]) xCount++;
+        }
+
+        PressCount.text = xCount.ToString() + "/" + playerCount.ToString();
+        Debug.Log(xCount);
+
+        if (xCount == playerCount)
+        {
+            Debug.Log("All four x pressed");
+        }
+    }
+    
+   
+}
